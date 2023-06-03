a1cfde4036e6c236707616207dad51487aa8be6e
Quang Hung Dang <hungdaqq@gmail.com>
2023-06-03 16:41:30 +0700

    Update app_main.c

--- main/app_main.c
+++ main/app_main.c
@@ -22,27 +22,28 @@
 #include "esp_smartconfig.h"
 #include "mqtt.h"
 
-static const char *TAG = "smartconfig_example";
+static const char *TAG = "smartconfig";
 static EventGroupHandle_t s_wifi_event_group;
 char status_topic[] = "messages/6c9c1971-6d20-41b3-ab49-fcb5f262e2e6/status";
 char update_topic[] = "messages/6c9c1971-6d20-41b3-ab49-fcb5f262e2e6/update";
 #define BUTTON_PIN GPIO_NUM_0  // Example button pin, change to match your setup
 
 
-#define QUEUE_LENGTH 5
-#define ITEM_SIZE sizeof(int)
 #define LED_PIN 2
-QueueHandle_t button_queue;
-TaskHandle_t heart;
-TaskHandle_t Led_TASK;
+
+TaskHandle_t heart;//heart dung de dinh chi hoac tiep tuc chay task send_heart
+TaskHandle_t Led_TASK;//heart dung de dinh chi hoac tiep tuc chay task led_task (task 1)
+
 bool old_button_state = false;
 bool button_state = true;
 gpio_config_t io_conf;
 static const int CONNECTED_BIT = BIT0;
 static const int ESPTOUCH_DONE_BIT = BIT1;
-uint8_t response = 0;
+static const int SMART_CONFIG_BIT = BIT3;
+static const int Led_TASK_BIT = BIT2;
+uint8_t response = 0;//bien response dung de check xem co ban tin {“code”: 1} gui ve khong
 static esp_mqtt_client_handle_t client;
-
+int led_state_1 = 0;
 void gpio_ouput_init(gpio_config_t *io_conf, uint8_t gpio_num){
     io_conf->mode = GPIO_MODE_OUTPUT;
     io_conf->pin_bit_mask = 1 << gpio_num;
@@ -51,6 +52,7 @@ void gpio_ouput_init(gpio_config_t *io_conf, uint8_t gpio_num){
     io_conf->intr_type = GPIO_INTR_DISABLE;
     gpio_config(io_conf);
 }
+
 esp_err_t mqtt_event_handler_cb(esp_mqtt_event_handle_t event)
 {   client = event->client;
     char *data;
@@ -63,6 +65,7 @@ esp_err_t mqtt_event_handler_cb(esp_mqtt_event_handle_t event)
             esp_mqtt_client_subscribe(client,status_topic, 0);
             break;
         case MQTT_EVENT_DISCONNECTED:
+            //khi mat ket noi mqtt thi tien hanh dinh chi 2 task nay de tranh lam treo chuong trinh
             vTaskSuspend(heart);
             vTaskSuspend(Led_TASK);
             ESP_LOGI(TAG, "MQTT_EVENT_DISCONNECTED");
@@ -84,10 +87,12 @@ esp_err_t mqtt_event_handler_cb(esp_mqtt_event_handle_t event)
             int gpio_led = 2;
             uint32_t led_state = 1;
             data = event->data;
+            //check xem data co phai la theo dinh dang {“code”: 1} khong.
             if(strstr(data,"code")){
                 response = 1;
             }
             else{ 
+                //kiem tra data co moi {“led”: {gpio_pin}, “status”: “{status_led}”} khong
                 if(strstr(data, "on")){
                     led_state = 1;
                 }
@@ -95,8 +100,10 @@ esp_err_t mqtt_event_handler_cb(esp_mqtt_event_handle_t event)
                     led_state = 0;
                 }
                 sscanf(data, "{\"led\": %d", &gpio_led);
+                //khoi tao chan gpio cho led
                 gpio_ouput_init(&io_conf,gpio_led);
                 gpio_set_level((uint8_t)gpio_led,led_state);
+                //tien hanh bat tat led nhu yeu cau
             }
             break;
         case MQTT_EVENT_ERROR:
@@ -108,6 +115,7 @@ esp_err_t mqtt_event_handler_cb(esp_mqtt_event_handle_t event)
     }
     return ESP_OK;
 }
+
 static void event_handler(void* arg, esp_event_base_t event_base, 
                                 int32_t event_id, void* event_data)
 {   
@@ -154,7 +162,7 @@ static void event_handler(void* arg, esp_event_base_t event_base,
 void button_task(void* parameter) {
     int button_press_count = 0;
     TickType_t last_button_press_time = 0;
-    int led_state_1 = 0;
+
     while (1) {
         int button_state = gpio_get_level(BUTTON_PIN);
 
@@ -169,11 +177,10 @@ void button_task(void* parameter) {
                 if (button_press_count == 2) {
                     printf("2 times\n");
                     led_state_1 = !led_state_1;
-                    xQueueSend(button_queue, &led_state_1, 0);  // Send LED state to the other task
+                    xEventGroupSetBits(s_wifi_event_group, Led_TASK_BIT);
                 } 
                 else if (button_press_count == 4) {
-                    int smart_config = 1;
-                    xQueueSend(button_queue, &smart_config, 0);  // Send LED state to the other task
+                    xEventGroupSetBits(s_wifi_event_group, SMART_CONFIG_BIT);
                     printf("4 times\n");
                     button_press_count = 0;
                 }
@@ -187,13 +194,11 @@ void button_task(void* parameter) {
     }
 }
 void led_task(void* parameter) {
-    int led_state_1 = 0;
-
     while (1) {
-        if (xQueueReceive(button_queue, &led_state_1, portMAX_DELAY) == pdTRUE) {
-            EventBits_t bits = xEventGroupGetBits(s_wifi_event_group);
-            if (bits & ESPTOUCH_DONE_BIT) {
-                xEventGroupClearBits(s_wifi_event_group, ESPTOUCH_DONE_BIT);
+        xEventGroupWaitBits(s_wifi_event_group, Led_TASK_BIT, true, false, portMAX_DELAY);
+            // kiem tra co ket noi toi internet chua, sau do thi moi publish du lieu
+        EventBits_t bits = xEventGroupGetBits(s_wifi_event_group);
+        if (bits & ESPTOUCH_DONE_BIT) {
                 gpio_set_level(LED_PIN, led_state_1);  // Set LED state based on the received value
                 char message[1024];
                 int led_pin = 2;
@@ -201,7 +206,6 @@ void led_task(void* parameter) {
                 int msg_id = esp_mqtt_client_publish(client, update_topic, message, 0, 0, 1);
                 ESP_LOGI(TAG, "Sent publish successful, msg_id=%d", msg_id);
                 ESP_LOGI(TAG,"Data:%d",led_state_1);
-            }
         }
     }
 }
@@ -225,15 +229,15 @@ void send_heart(void* parameter) {
 Ham smart_config dung duoc goi khi bam 4 lan nut lan va se vao qua trinh smartconfig
 */
 void smart_config(void* parameter) {
-    int smart_config = 0;
     while (1) {
-        if (xQueueReceive(button_queue, &smart_config, portMAX_DELAY) == pdTRUE) {
+        xEventGroupWaitBits(s_wifi_event_group, SMART_CONFIG_BIT, true, false, portMAX_DELAY);
             EventBits_t bits = xEventGroupGetBits(s_wifi_event_group);
             //Kiem tra neu co truoc noi tu truoc do thi se ngat ket noi mqtt
             if (bits & ESPTOUCH_DONE_BIT) {
                 esp_mqtt_client_stop(client);
                 xEventGroupClearBits(s_wifi_event_group, ESPTOUCH_DONE_BIT);
             }
+            //ngat ket noi wifi, ngat smartconfig neu gia su chuong trinh truoc do da ket noi wifi hoac dang chay smart config
             esp_wifi_stop();
             esp_smartconfig_stop();
             ESP_ERROR_CHECK( esp_wifi_start() );
@@ -244,12 +248,12 @@ void smart_config(void* parameter) {
             xEventGroupWaitBits(s_wifi_event_group, ESPTOUCH_DONE_BIT, false, false, portMAX_DELAY);
             esp_smartconfig_stop();
             mqtt_app_start();
-        }
     }
 }
-
+//Ham init_wifi() khoi tao cac event loop cho wifi, khoi tao cau hinh cho wifi
 void init_wifi(){
     ESP_ERROR_CHECK(esp_netif_init());
+    //tao event_group
     s_wifi_event_group = xEventGroupCreate();
     ESP_ERROR_CHECK(esp_event_loop_create_default());
     esp_netif_t *sta_netif = esp_netif_create_default_wifi_sta();
@@ -267,14 +271,17 @@ void app_main(void)
     //Khoi tao thong tin wifi ban dau
     init_wifi();
     gpio_ouput_init(&io_conf,LED_PIN);
-    //tao 1 queue de xu ly nut nhan
-    button_queue = xQueueCreate(QUEUE_LENGTH, ITEM_SIZE);
+    //task button_task la task xu ly nut bam
     xTaskCreate(button_task, "button_task", 2048, NULL, 10, NULL);
+    //task led_task la task xu ly task1 trong de bai
     xTaskCreate(led_task, "led_task", 4096, NULL, 10, &Led_TASK);
-    vTaskSuspend(Led_TASK);
+    //task smart_config dung de xu ly smart config
     xTaskCreate(smart_config, "smart_config", 2048, NULL, 10, NULL);
+    //task send_heart dung de xu ly task3 trong de bai
     xTaskCreate(send_heart, "send_heart", 2048, NULL, 11, &heart);
+    //tam thoi dinh chi task send_heart cho toi khi co ket noi mqtt thanh cong
     vTaskSuspend(heart);
+    //set ham call back xu ly mqtt event
     set_mqtt_callback_event(mqtt_event_handler_cb);
 }
 
